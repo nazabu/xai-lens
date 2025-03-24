60abe92c926ee9d17f91d374346d31e3c98599b4
nazabu <nazrievabubakr2007@gmail.com>
2025-03-23 22:40:55 -0600

    Update scoring.py

--- xai_lens/scoring.py
+++ xai_lens/scoring.py
@@ -4,5 +4,5 @@ class InterpretabilityScorer:
     def __init__(self, model, data=None):
         self.model = model
         self.data = data
-        
+        self.model_type  = None # model type function
 
